ce1704862e0df0d8c41cab2203308d55ec30af0c
Deepak <deepaksahoodeveloper@gmail.com>
2024-04-29 01:33:30 +0530

    add app littlelemon_api, templates and static/restaurant/logo.png

--- littlelemon_proj/littlelemon_proj/settings.py
+++ littlelemon_proj/littlelemon_proj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'littlelemon_api',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'littlelemon_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
