463262c5879e4ed22b5ec342588395b898a4cd83
Deepak <deepaksahoodeveloper@gmail.com>
2024-05-01 00:20:24 +0530

    Set up user registration and authentication

--- littlelemon_proj/littlelemon_api/views.py
+++ littlelemon_proj/littlelemon_api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Booking, Menu 
 from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -10,17 +11,21 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class MenuRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class BookingListCreateAPIView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 class BookingRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
