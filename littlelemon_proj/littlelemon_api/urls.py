2a9e48cb3440a60e343f6e292422bb2970268860
Deepak <deepaksahoodeveloper@gmail.com>
2024-05-01 02:12:28 +0530

    Update README.md

--- littlelemon_proj/littlelemon_api/urls.py
+++ littlelemon_proj/littlelemon_api/urls.py
@@ -4,7 +4,7 @@ from .views import index , MenuListCreateAPIView, MenuRetrieveUpdateDestroyAPIVi
   
 urlpatterns = [ 
      path('', index, name='index'),
-     path('menu/', MenuListCreateAPIView.as_view()),
+     path('menu', MenuListCreateAPIView.as_view()),
      path('menu/<int:pk>', MenuRetrieveUpdateDestroyAPIView.as_view()),
      path('booking', BookingListCreateAPIView.as_view()),
      path('booking/<int:pk>', BookingRetrieveUpdateDestroyAPIView.as_view()),
